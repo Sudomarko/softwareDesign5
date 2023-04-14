632c8793f69daa9cceb0a6dc3c886124135885ad
Sudomarko <cigamali007@gmail.com>
2023-04-15 01:41:38 +0200

    done w everythinh

--- assignment5/src/assignment5/Student.java
+++ assignment5/src/assignment5/Student.java
@@ -14,14 +14,16 @@ public class Student {
 	public ArrayList<String> events;
 	public SchedFact fact = new SchedFact();
 	public MentalHealth mental;
+	public AcademicApplication advisor;
 	
-	public Student(String firstName, String lastName, String password) {
+	public Student(String firstName, String lastName, String password, AcademicApplication advisor) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.password = password;	
 		this.organisations = new ArrayList<Organisation>();
 		this.sched = fact.getSched(this);
 		this.security = new SocialApplication();
+		this.advisor = advisor;
 	}
 	
 	public boolean editLastName(String newName) {
@@ -68,6 +70,12 @@ public class Student {
 	
 	public void assignMH(MentalHealth mh) {
 		this.mental = mh;
+	}
+	
+	public void schedAdvisor(String date) {
+		this.events.add(date);
+		this.advisor.aSched.addEvent(date);
+		System.out.println(this.advisor.aSched.getEvents()); //Filter to events that only apply to current student
 	}
 	
 	public void setMHApt(String date) {
